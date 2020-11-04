e87a635ce0793fc01477f9abc280bdb90651c9f8
DanielleInkster <danielle.inkster@gmail.com>
2020-11-04 16:24:36 -0500

    added example logins

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
+  authenticated :user do
+    root "pages#my_ideas_lists", as: :authenticated_root
+  end
   root 'pages#home'
   get 'pages/my_ideas_lists'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
